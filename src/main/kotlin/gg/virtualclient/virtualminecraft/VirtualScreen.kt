d0c81e6f552ee450d3643add47639fa4b0f3a5e9
David Mayr <davidliebtkekse@gmail.com>
2022-07-18 17:54:32 +0200

    Moved every mouse event to seperate method so virtualgui works

--- src/main/kotlin/gg/virtualclient/virtualminecraft/VirtualScreen.kt
+++ src/main/kotlin/gg/virtualclient/virtualminecraft/VirtualScreen.kt
@@ -31,43 +31,83 @@ abstract class VirtualScreen(title: Component) : Screen(title.asMinecraft()) {
         //#endif
     }
 
-    override fun init() {
+    final override fun init() {
+        onScreenInit()
+    }
+
+    open fun onScreenInit() {
         super.init()
     }
 
-    override fun keyPressed(keyCode: Int, scanCode: Int, modifiers: Int): Boolean {
+    final override fun keyPressed(keyCode: Int, scanCode: Int, modifiers: Int): Boolean {
+        return onKeyPressed(keyCode, scanCode, modifiers)
+    }
+
+    open fun onKeyPressed(keyCode: Int, scanCode: Int, modifiers: Int): Boolean {
         return super.keyPressed(keyCode, scanCode, modifiers)
     }
 
-    override fun keyReleased(keyCode: Int, scanCode: Int, modifiers: Int): Boolean {
+    final override fun keyReleased(keyCode: Int, scanCode: Int, modifiers: Int): Boolean {
+        return onKeyReleased(keyCode, scanCode, modifiers)
+    }
+
+    open fun onKeyReleased(keyCode: Int, scanCode: Int, modifiers: Int): Boolean {
         return super.keyReleased(keyCode, scanCode, modifiers)
     }
 
-    override fun charTyped(chr: Char, modifiers: Int): Boolean {
+    final override fun charTyped(chr: Char, modifiers: Int): Boolean {
+        return onCharTyped(chr, modifiers)
+    }
+
+    open fun onCharTyped(chr: Char, modifiers: Int): Boolean {
         return super.charTyped(chr, modifiers)
     }
 
-    override fun mouseClicked(mouseX: Double, mouseY: Double, button: Int): Boolean {
+    final override fun mouseClicked(mouseX: Double, mouseY: Double, button: Int): Boolean {
+        return onMouseClicked(mouseX, mouseY, button)
+    }
+
+    open fun onMouseClicked(mouseX: Double, mouseY: Double, button: Int): Boolean {
         return super.mouseClicked(mouseX, mouseY, button)
     }
 
-    override fun mouseReleased(mouseX: Double, mouseY: Double, button: Int): Boolean {
+    final override fun mouseReleased(mouseX: Double, mouseY: Double, button: Int): Boolean {
+        return onMouseReleased(mouseX, mouseY, button)
+    }
+
+    open fun onMouseReleased(mouseX: Double, mouseY: Double, button: Int): Boolean {
         return super.mouseReleased(mouseX, mouseY, button)
     }
 
-    override fun mouseDragged(mouseX: Double, mouseY: Double, button: Int, deltaX: Double, deltaY: Double): Boolean {
+    final override fun mouseDragged(mouseX: Double, mouseY: Double, button: Int, deltaX: Double, deltaY: Double): Boolean {
+        return onMouseDragged(mouseX, mouseY, button, deltaX, deltaY)
+    }
+
+    open fun onMouseDragged(mouseX: Double, mouseY: Double, button: Int, deltaX: Double, deltaY: Double): Boolean {
         return super.mouseDragged(mouseX, mouseY, button, deltaX, deltaY)
     }
 
-    override fun mouseScrolled(mouseX: Double, mouseY: Double, amount: Double): Boolean {
+    final override fun mouseScrolled(mouseX: Double, mouseY: Double, amount: Double): Boolean {
+        return onMouseScrolled(mouseX, mouseY, amount)
+    }
+
+    open fun onMouseScrolled(mouseX: Double, mouseY: Double, amount: Double): Boolean {
         return super.mouseScrolled(mouseX, mouseY, amount)
     }
 
-    override fun tick() {
+    final override fun tick() {
+        onTick()
+    }
+
+    open fun onTick() {
         super.tick()
     }
 
-    override fun close() {
+    final override fun close() {
+        onScreenClosed()
+    }
+
+    open fun onScreenClosed() {
         super.close()
     }
 
