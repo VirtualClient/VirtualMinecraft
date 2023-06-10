747c615c0a43c8c5392e5c629d257ec529a072f6
David Mayr <davidliebtkekse@gmail.com>
2023-06-10 19:02:32 +0200

    build: bump version

--- minecraft-versions/version.gradle.kts
+++ minecraft-versions/version.gradle.kts
@@ -41,7 +41,7 @@ configure<PublishingExtension> {
         create<MavenPublication>("maven") {
             groupId = "gg.virtualclient"
             artifactId = "virtualminecraft"
-            version = "1.0.3-${platform.mcVersion}-SNAPSHOT"
+            version = "1.0.4-${platform.mcVersion}-SNAPSHOT"
             from(components["java"])
             artifact(tasks.jar)
         }
